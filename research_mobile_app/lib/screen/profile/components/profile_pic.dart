afe34835369a7f63e94c478eebd07b1b6559822a
IT20665852 <it20665852@my.sliit.lk>
2024-03-14 22:55:42 +0530

    create profile UI and search UI

---
+++ research_mobile_app/lib/screen/profile/components/profile_pic.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ProfilePic extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser!;
+  
+  ProfilePic(String? photoURL, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      width: 130,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(user.photoURL!),
+          ),
+          Positioned(
+            right: -16,
+            bottom: 0,
+            child: SizedBox(
+              height: 46,
+              width: 46,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white, shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    side: const BorderSide(color: Colors.white),
+                  ),
+                  backgroundColor: const Color(0xFFF5F6F9),
+                ),
+                onPressed: () {},
+                child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ research_mobile_app/lib/screen/profile/profile_screen.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:melowave1/config/theme/app_bar.dart';
+import 'components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser;
+
+  static String routeName = '/profile';
+
+  ProfileScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: "Profile",
+        leadingImage: null, // Pass the user's photo URL directly
+        actionImage: null,
+        onLeadingPressed: () {
+          print("Leading icon pressed");
+        },
+        onActionPressed: () {
+          print("Action icon pressed");
+        },
+      ),
+      body: Body(user: user),
+    );
+  }
+}
